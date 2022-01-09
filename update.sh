05808d040d9142382a80701f1bce2016f3eca888
pederbirk <peder.birk@gmail.com>
2022-01-09 15:23:58 +0100

    added script to copy config

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp ~/.config/qtile ~/Repos/SystemSetup -r      
+cp ~/.config/rofi ~/Repos/SystemSetup -r 
+cp ~/.config/kitty ~/Repos/SystemSetup -r 
+cp ~/.config/conky ~/Repos/SystemSetup -r 
