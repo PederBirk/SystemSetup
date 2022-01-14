9250f3dcf8e0591ea5d1f1704d691636a94c34bf
pederbirk <peder.birk@gmail.com>
2022-01-14 15:48:27 +0100

    update

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ cp ~/.config/rofi ~/Repos/SystemSetup -r
 cp ~/.config/kitty ~/Repos/SystemSetup -r 
 cp ~/.config/conky ~/Repos/SystemSetup -r
 cp ~/.config/starship.toml ~/Repos/SystemSetup
+cp /usr/share/sddm/themes/arc ~/Repos/SystemSetup/SddmThemes -r
+cp /etc/sddm.conf ~/Repos/SystemSetup
